b016aaace3cfef39a6b25010816de998735aa2ed
Prins Naval Nuzeren <100395775+prinsereen@users.noreply.github.com>
2023-09-24 11:43:13 +0700

    [ADD] get User ById

--- controllers/User.js
+++ controllers/User.js
@@ -3,10 +3,46 @@ import Users from "../models/UserModel.js";
 export const getAllUsers = async(req, res) => {
     try {
         const users = await Users.findAll({
-            attributes: ['id', 'jenis_pengguna', 'nik']
+            attributes: [
+            'id', 
+            'jenis_pengguna', 
+            'nik',
+            'riwayat_penyakit',
+            'riwayat_alergi',
+            'berat_badan',
+            'tinggi_badan',
+            'tekanan_darah',
+            'denyut_jantung',
+            'catatan_tambahan'
+        ]
         });
         res.json(users)
     } catch (error) {
-        console.log(error)
+        res.status(500).json({ msg: error.message });
+    }
+}
+
+export const getUserById = async(req, res) => {
+    try {
+        const user = await Users.findOne({
+        attributes: [
+            'id', 
+            'jenis_pengguna', 
+            'nik',
+            'riwayat_penyakit',
+            'riwayat_alergi',
+            'berat_badan',
+            'tinggi_badan',
+            'tekanan_darah',
+            'denyut_jantung',
+            'catatan_tambahan'
+        ], 
+            where: {
+                id: req.params.id 
+            }
+        })
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
     }
 }
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { verifyToken } from "../middleware/verifyToken.js";
-import { getAllUsers } from "../controllers/User.js";
+import { getAllUsers, getUserById } from "../controllers/User.js";
 
 const router = express.Router();
 
 router.get('/users', verifyToken, getAllUsers)
+router.get('/users/:id', verifyToken, getUserById)
 
 export default router;

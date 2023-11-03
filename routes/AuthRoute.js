4d220e6c18bf4cf0b343608a5ca4549bc38622d6
Prins Naval Nuzeren <100395775+prinsereen@users.noreply.github.com>
2023-11-03 10:19:57 +0700

    [ADD] add validation and several function

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import {register, login, logout} from "../controllers/Auth.js"
 import { refreshToken } from "../controllers/RefreshToken.js";
+import { Register, Login } from "../validation/AuthValidation.js";
 
 const router = express.Router();
 
-router.post('/register', register)
-router.post('/login', login)
+router.post('/register', Register, register)
+router.post('/login', Login, login)
 router.get('/token', refreshToken)
 router.delete('/logout', logout)
 

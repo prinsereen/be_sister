9200f8a1ab7e02e8fcd1e3c80291b0be35cffa9f
Prins Naval Nuzeren <100395775+prinsereen@users.noreply.github.com>
2023-12-24 13:34:30 +0700

    deploy 2

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-import express from "express"
+import express from "express";
 import db from "./config/Database.js";
-import EventModel from "./models/EventModel.js"
-import EventRoute from "./routes/EventRoute.js"
+import EventModel from "./models/EventModel.js";
+import EventRoute from "./routes/EventRoute.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"; // Import cors middleware
 
 const app = express();
 
 try {
     await db.authenticate();
-    console.log("Database Connected ...")
-    await db.sync()
+    console.log("Database Connected ...");
+    await db.sync();
 } catch (error) {
-    console.log(error)
+    console.log(error);
 }
 
-app.use(cookieParser())
-app.use(express.json())
-app.use(EventRoute)
+app.use(cors()); 
+app.use(cookieParser());
+app.use(express.json());
+app.use(EventRoute);
 
-app.listen(5000, ()=> console.log("server running on port 5000"))
+app.listen(5000, () => console.log("Server running on port 5000"));
